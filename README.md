f6eaaf0b11649b33fc958ff934a26b8d9595cd23
Thomas Ryan <thomasryan2118@gmail.com>
2023-06-28 19:53:08 -0500

    API request button working for default of New York

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This repository contains code for a simple weather prediction application.
+
+Sources: 
+https://www.youtube.com/watch?v=xOQxkkw_GF0 - For hiding an API key
--- app.js
+++ app.js
@@ -3,13 +3,20 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
+require("dotenv").config();
+
 app.use(cors({
     origin: 'http://127.0.0.1:3000',
     optionsSuccessStatus : 200
 }))
 
 app.get('/', (req, res) => {
-    fetch('https://api.weatherstack.com/current')
+    console.log(process.env.API_KEY);
+    var params = {
+        access_key : process.env.API_KEY,
+        query : "New York"
+    }
+    fetch(`http://api.weatherstack.com/current?access_key=${process.env.API_KEY}&query=New York`)
     .then(resObj => resObj.text())
     .then(data => {
         res.json(data);
