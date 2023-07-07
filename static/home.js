66e2318d12593dec041832987367026ac4b95f79
Thomas Ryan <thomasryan2118@gmail.com>
2023-07-06 20:04:39 -0500

    present more data to user in basic format

--- static/home.js
+++ static/home.js
@@ -1,7 +1,6 @@
 
 $(function () {
     $("#submit").on("click", function (e) {
-        alert("submit button press")
         $.ajax({
             type: "POST",
             url: "http://localhost:3000/submit",
@@ -11,7 +10,18 @@ $(function () {
             },
             success: function (result, status, xhr) {
                 var jsonResult = JSON.stringify(result);
+                var response = JSON.parse(result);
                 $("#results").text(jsonResult);
+                $("#prettyResults").append(`<h1> ${response.location.name}, ${response.location.region} </h1>`);
+                var date = new Date(response.location.localtime.substring(0, 10));
+                const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+                const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+                alert(date.getDay());
+                alert(date.getMonth());
+                alert(date.getTime());
+                $("#prettyResults").append(`<h2> ${weekdays[date.getDay()]}, ${months[date.getMonth()]} ${date.getDate()} ${response.current.observation_time}`);
+                $("#prettyResults").append(`<p> The weather is currently ${response.current.weather_descriptions} and ${response.current.temperature} degrees`);
+                $("#prettyResults").append(`<img src="${response.current.weather_icons}"`);
             },
             error: function (xhr, status, error) {
                 alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText);
