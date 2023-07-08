f1399271686f4fb5002c830812515bb2b310b6eb
Thomas Ryan <thomasryan2118@gmail.com>
2023-07-08 14:16:43 -0500

    centered city label

--- static/home.js
+++ static/home.js
@@ -12,14 +12,16 @@ $(function () {
                 var jsonResult = JSON.stringify(result);
                 var response = JSON.parse(result);
                 $("#results").text(jsonResult);
-                $("#prettyResults").append(`<h1> ${response.location.name}, ${response.location.region} </h1>`);
+                $("#prettyResults").append(`<h1 id="city"> ${response.location.name}, ${response.location.region} </h1>`);
                 var date = new Date(response.location.localtime.substring(0, 10));
                 const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
                 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-                alert(date.getDay());
-                alert(date.getMonth());
-                alert(date.getTime());
-                $("#prettyResults").append(`<h2> ${weekdays[date.getDay()]}, ${months[date.getMonth()]} ${date.getDate()} ${response.current.observation_time}`);
+
+                var observation_time = response.current.observation_time;
+                if (observation_time[0] == '0') {
+                    observation_time = observation_time.substring(1);
+                }
+                $("#prettyResults").append(`<h2> ${weekdays[date.getDay()]}, ${months[date.getMonth()]} ${date.getDate()} ${observation_time}`);
                 $("#prettyResults").append(`<p> The weather is currently ${response.current.weather_descriptions} and ${response.current.temperature} degrees`);
                 $("#prettyResults").append(`<img src="${response.current.weather_icons}"`);
             },
