8e16d2e8175ba65d1671d261cfd248f7e56f41b5
Thomas Ryan <thomasryan2118@gmail.com>
2023-11-13 11:56:55 -0600

    app now displays correct local time instead of UTC

--- static/home.js
+++ static/home.js
@@ -15,17 +15,26 @@ $(function () {
 
                 //clear previous results in the case of multiple searches
                 $("#prettyResults").empty()
-                
+
                 $("#prettyResults").append(`<h1 id="city"> ${response.location.name}, ${response.location.region} </h1>`);
-                var date = new Date(response.location.localtime.substring(0, 10));
-                const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+                var currDate = new Date(response.location.localtime);
+                console.log(response.location.localtime.substring(0, 10))
+                const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
                 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-                var observation_time = response.current.observation_time;
+                var observation_time = response.location.localtime.substring(11);
+                if (observation_time.substring(0,2) >= '12') {
+                    observation_time = observation_time + ' PM';
+                }
+                else {
+                    observation_time = observation_time + ' AM';
+                }
                 if (observation_time[0] == '0') {
                     observation_time = observation_time.substring(1);
                 }
-                $("#prettyResults").append(`<h2> ${weekdays[date.getDay()]}, ${months[date.getMonth()]} ${date.getDate()} ${observation_time}`);
+
+                $("#prettyResults").append(`<h2> ${weekdays[currDate.getDay()]}, ${months[currDate.getMonth()]} ${currDate.getDate()} ${observation_time}`);
+                console.log(currDate)
                 $("#prettyResults").append(`<p> The weather is currently ${response.current.weather_descriptions} and ${response.current.temperature} degrees`);
                 
                 if (JSON.stringify(response.current.weather_descriptions).toLowerCase().indexOf("sunny") !== -1) {
